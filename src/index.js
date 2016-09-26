3b8360797ab56833f4884f1bd08e735b76a5614d
troy.tao <troy.tao@rangle.io>
2016-09-26 13:59:09 -0400

    Simplify the index.js to display 'Hello World'

--- src/index.js
+++ src/index.js
@@ -4,27 +4,22 @@ import 'es6-promise';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-
-import routes from './store/routes';
-import configureStore from './store/configure-store';
 
 import './styles/index.css';
 
-const store = configureStore({});
-const history = syncHistoryWithStore(browserHistory, store);
+
+const Hello = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+  },
+  render: function render() {
+    return <div>Hello {this.props.name}</div>;
+  },
+});
 
 if (!__TEST__) {
   ReactDOM.render(
-    <div>
-      <Provider store={ store }>
-        <Router history={ history }>
-          { routes }
-        </Router>
-      </Provider>
-    </div>,
+    <Hello name="World" />,
     document.getElementById('root')
   );
 }
