0dfdcec291609afa60668aa0961a5e1b987fadce
troy.tao <troy.tao@rangle.io>
2016-09-26 14:11:20 -0400

    Remove src/components & src/containers

--- src/store/routes.js
+++ src/store/routes.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import App from '../containers/app';
-import AboutPage from '../containers/about-page';
-import CounterPage from '../containers/counter-page';
 
 export default (
   <Route path="/" component={ App }>
