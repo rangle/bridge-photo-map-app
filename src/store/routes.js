5988561a492f8633fd2f3fc9f1ad6942e5544856
brainstation <clearlythuydoan@gmail.com>
2017-02-20 21:07:00 -0500

    adjust route for folder and app.js name change

--- src/store/routes.js
+++ src/store/routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from '../components/App/App';
+import PhotoGallery from '../containers/photo-gallery';
 
 export default (
-  <Route path="/" component={ App }/>
+  <Route path="/" component={ PhotoGallery }/>
 );
