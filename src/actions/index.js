34e4ff1afa4397a2dca41c476dc9948589f167bc
megseoh <ohara.meagan@gmail.com>
2017-02-21 19:23:42 -0500

    related photos displaying based on hardcoded geolocation under map

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,12 @@ export const ACTION_TYPES = {
   setSelectedPhotoID: 'SET_SELECTED_PHOTO_ID',
   showInfoWindow: 'SHOW_INFO_WINDOW',
   setActiveMarker: 'SET_ACTIVE_MARKER',
+<<<<<<< HEAD
   searchPhotos: 'SEARCH_PHOTOS',
   handleInput: 'HANDLE_INPUT',
+=======
+  getRelatedPhotos: 'GET_RELATED_PHOTOS',
+>>>>>>> related photos displaying based on hardcoded geolocation under map
 };
 
 // This handles loading photos on mount. Temporary.
@@ -74,6 +78,20 @@ export function searchPhotos(keyword) {
         payload: {
           search: response.photos,
           status: true,
+        },
+      });
+    });
+  };
+}
+
+export function getRelatedPhotos() {
+  return dispatch => {
+    get('/photos/search', { geo: '43.6532,-79.3832,1km' })
+    .then(response => {
+      dispatch({
+        type: ACTION_TYPES.getRelatedPhotos,
+        payload: {
+          photos: response.photos,
         },
       });
     });
