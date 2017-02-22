d77c22cb704d7d00f160f34856ac812eecf34ae2
brainstation <clearlythuydoan@gmail.com>
2017-02-21 19:19:56 -0500

    fix routes for central app component

--- src/store/routes.js
+++ src/store/routes.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from '../components/App/App';
-import PhotoGallery from '../containers/photo-gallery';
 
 export default (
-  <Route path="/" component={ PhotoGallery }/>
+  <Route path="/" component={ App }/>
 );
