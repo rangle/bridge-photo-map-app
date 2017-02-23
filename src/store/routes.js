bb8949ac30b1b54f324260941a3cf8195847cf57
brainstation <clearlythuydoan@gmail.com>
2017-02-23 18:25:17 -0500

    new route path for image details

--- src/store/routes.js
+++ src/store/routes.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import App from '../components/App/App';
+import PhotoDetails from '../components/PhotoGallery/PhotoDetails';
 
 export default (
-  <Route path="/" component={ App }/>
+  <Router history={ browserHistory }>
+    <Route path="/" component={ App }/>
+    <Route path="/details/:id" component={PhotoDetails}/>
+  </Router>
 );
